a7f3d4d412426e255e3bf4892ecb6822e242bdcc
Mardigras30 <123905093+Mardigras30@users.noreply.github.com>
2023-04-22 21:28:12 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+function generateDomianName() {
+  let randomPronounIndex = Math.floor(Math.random() * pronoun.length);
+  let randomAdjIndex = Math.floor(Math.random() * adj.length);
+  let randomNounIndex = Math.floor(Math.random() * noun.length);
+  let randomExtIndex = Math.floor(Math.random() * ext.length);
+
+  let generateDomianName = pronoun[randomPronounIndex] + adj[randomAdjIndex] + noun[randonNounIndex] + ext[randomExtIndex];
+
+  console.log(domainName);
+
+  document.getElementById("domainName").textContent = domainName;
+}
   console.log("Hello Rigo from the console!");
 };
+
